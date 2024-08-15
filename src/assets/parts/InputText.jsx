b5013ab1c58ea6ed7aa80f3aa3512049c8e5aaa8
Koray Demir <koraydemirmc@gmail.com>
2024-08-15 15:50:00 +0300

    Çevrilen dilin animasyonlu yazılması eklendi

--- src/assets/parts/InputText.jsx
+++ src/assets/parts/InputText.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Input } from 'antd';
 const { TextArea } = Input;
 
-const InputText = ({ text, disabled, translate }) => {
+const InputText = ({ text, disabled, translate, translatedText }) => {
   const [value, setValue] = useState('');
+  const [displayedText, setDisplayedText] = useState('');
+
+  useEffect(() => {
+    if (translatedText) {
+      setDisplayedText('');
+      setValue(''); // Değişiklikleri sil
+      let index = 0;
+      const interval = setInterval(() => {
+        setDisplayedText(prev => prev + translatedText[index]);
+        index += 1;
+        if (index >= translatedText.length) {
+          clearInterval(interval);
+        }
+      }, 35); // Hızını ayarla
+      return () => clearInterval(interval);
+    } else {
+      setDisplayedText('');
+    }
+  }, [translatedText]);
 
   const handleChange = (e) => {
     const newValue = e.target.value;
     setValue(newValue);
-    translate(newValue);
+    newValue && translate(newValue);
   };
 
   return (
     <TextArea
-      value={value}
+      value={translatedText ? displayedText : value}
       onChange={handleChange}
       placeholder={text}
       autoSize={{
